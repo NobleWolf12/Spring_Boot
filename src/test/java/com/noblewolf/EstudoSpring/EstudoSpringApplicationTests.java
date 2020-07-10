682a55c432f4c8573e5103ddf8cbd80a378bca5e
Adolfo Gotler <adolfogotler@gmail.com>
2020-07-10 16:04:07 -0300

    Projeto Criado

---
+++ src/test/java/com/noblewolf/EstudoSpring/EstudoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noblewolf.EstudoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
